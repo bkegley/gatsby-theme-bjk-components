3b1b5e79f4352b03102689960459b1bb9eb1a2ad
Bryan Kegley <hi@bryankegley.me>
2019-07-12 22:37:20 -0500

    testing in example

--- example/src/pages/index.js
+++ example/src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import {Checkbox} from 'gatsby-theme-bjk-components'
+import {Checkbox, Dropdown, TextInput, TextArea} from 'gatsby-theme-bjk-components'
+import {Formik, Form} from 'formik'
 
 const IndexPage = () => {
   const [checked, setChecked] = React.useState(false)
@@ -10,6 +11,33 @@ const IndexPage = () => {
         <p>This is the home page</p>
       </div>
       <Checkbox toggleCheckbox={() => setChecked(old => !old)} checked={checked} />
+      <Formik initialValues={{input: 'hey', textarea: 'hey there you guys'}}>
+        {({values}) => {
+          return (
+            <div>
+              <Form>
+                <TextInput formik name="input" />
+                <div style={{width: '100%'}}>
+                  <TextArea formik name="textarea" rows={10} />
+                </div>
+                <div>
+                  <Dropdown
+                    formik
+                    name="stage"
+                    label="Stage"
+                    options={[
+                      {text: 'Lead', value: 'lead'},
+                      {text: 'Lost', value: 'lost'},
+                      {text: 'Current', value: 'current'},
+                    ]}
+                  />
+                </div>
+              </Form>
+              <pre>{JSON.stringify(values, null, 2)}</pre>
+            </div>
+          )
+        }}
+      </Formik>
     </div>
   )
 }
