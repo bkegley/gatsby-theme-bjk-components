bbac31940c1244ce7ff537459f18c6cca998ffd9
Bryan Kegley <hi@bryankegley.me>
2019-07-14 14:53:01 -0500

    implement session save

--- example/src/pages/index.js
+++ example/src/pages/index.js
@@ -9,23 +9,16 @@ import useLocalStorage from '../hooks/useLocalStorage'
 import {Dropdown, Modal} from 'gatsby-theme-bjk-components'
 import CoffeeCreateForm from '../components/CoffeeCreateForm'
 
-const BrewButton = () => {
-  const {toggleTimer, isRunning} = useTimer()
-  return (
-    <Button onClick={toggleTimer}>
-      <h1>{isRunning ? 'Stop' : 'Start'}</h1>
-    </Button>
-  )
-}
+const convertSecondsToTimeString = seconds => new Date(seconds * 1000).toISOString().substr(14, 5)
 
 const SectionOne = ({style, setSection}) => {
-  const {isRunning} = useTimer()
+  const {session} = useTimer()
 
   React.useEffect(() => {
-    if (isRunning) setSection(2)
+    if (session) setSection(2)
   }, [])
   return (
-    <AnimatedBox style={style} sx={{position: 'absolute'}}>
+    <AnimatedBox style={style} sx={{position: 'absolute', top: '30vh'}}>
       <Box sx={{textAlign: 'center'}}>
         <Box sx={{fontSize: 6}}>Welcome to </Box>
         <Box sx={{fontSize: 8, color: 'primary', mb: 5}}>brewd</Box>
@@ -37,8 +30,8 @@ const SectionOne = ({style, setSection}) => {
   )
 }
 
-const SectionTwo = ({style, setSection, setCoffee}) => {
-  const [coffees, setCoffees] = useLocalStorage('coffees', [])
+const SectionTwo = ({style, setSection, coffee, setCoffee}) => {
+  const [localStorageCoffees, setLocalStorageCoffees] = useLocalStorage('coffees', [])
   const [showModal, setShowModal] = React.useState(false)
   const toggleModal = () => setShowModal(old => !old)
   return (
@@ -46,22 +39,27 @@ const SectionTwo = ({style, setSection, setCoffee}) => {
       {showModal ? (
         <Modal onClickOutside={toggleModal}>
           <Box sx={{p: 4}}>
-            <CoffeeCreateForm coffees={coffees} setCoffees={setCoffees} onClickOutside={toggleModal} />
+            <CoffeeCreateForm
+              coffees={localStorageCoffees}
+              setCoffees={setLocalStorageCoffees}
+              onClickOutside={toggleModal}
+            />
           </Box>
         </Modal>
       ) : null}
-      <AnimatedBox style={style} sx={{position: 'absolute'}}>
+      <AnimatedBox style={style} sx={{position: 'absolute', top: '30vh'}}>
         <Box sx={{textAlign: 'center'}}>
           <Box sx={{fontSize: 6}}>pick a coffee</Box>
           <Box sx={{my: 4}}>
             <Dropdown
-              onChange={selection => console.log({selection})}
+              onChange={selection => setCoffee(selection ? selection.value : null)}
               onCreateNew={toggleModal}
               name="coffee"
-              options={coffees.map(coffee => ({text: coffee.name, value: coffee.id}))}
+              initialSelectedItem={coffee}
+              options={localStorageCoffees.map(coffee => ({text: coffee.name, value: coffee.id}))}
             />
           </Box>
-          <Button type="button" onClick={() => setSection(2)}>
+          <Button type="button" disabled={coffee ? false : true} onClick={() => setSection(2)}>
             Let's go!
           </Button>
         </Box>
@@ -70,14 +68,65 @@ const SectionTwo = ({style, setSection, setCoffee}) => {
   )
 }
 
-const SectionThree = ({style, setSection}) => {
+const SectionThree = ({style, coffee}) => {
+  const [sessions, setSessions] = useLocalStorage('sessions', [])
+  const {time, isRunning, session, startSession, toggleTimer, resetTimer, resetSession} = useTimer()
+
+  const handleSubmit = () => {
+    setSessions(
+      sessions.concat({
+        ...session,
+        ...time,
+      }),
+    )
+    resetTimer()
+    resetSession()
+  }
   return (
-    <AnimatedBox style={style} sx={{position: 'absolute'}}>
+    <AnimatedBox style={style} sx={{position: 'absolute', top: '30vh'}}>
       <Box sx={{textAlign: 'center'}}>
-        <Box sx={{fontSize: 8, color: 'primary', mb: 5}}>ready?</Box>
-        <BrewButton type="button" onClick={() => setSection(1)}>
-          Let's go!
-        </BrewButton>
+        <Box sx={{fontSize: 8, color: 'primary', mb: 5}}>
+          {isRunning ? convertSecondsToTimeString(time.totalTime) : session ? 'save?' : 'ready?'}
+        </Box>
+        {session && !isRunning ? (
+          <Box>
+            <Button onClick={handleSubmit}>
+              <h1>Save</h1>
+            </Button>
+          </Box>
+        ) : (
+          <Box>
+            <Button
+              onClick={() => {
+                toggleTimer()
+                if (!session) startSession({id: sessions.length + 1, coffee})
+              }}
+            >
+              <h1>{isRunning ? 'Stop' : 'Start'}</h1>
+            </Button>
+          </Box>
+        )}
+        {session && !isRunning ? (
+          <Flex sx={{mt: 5, flexDirection: ['column', 'column', 'row', 'row'], alignItems: 'center'}}>
+            <Box sx={{mx: [0, 0, 4, 4], my: [3, 3, 0, 0]}}>
+              <Button type="button" variant="secondary" onClick={toggleTimer}>
+                Continue
+              </Button>
+            </Box>
+            <Box sx={{mx: [0, 0, 4, 4], my: [3, 3, 0, 0]}}>
+              <Button
+                type="button"
+                variant="secondary"
+                onClick={() => {
+                  resetTimer()
+                  resetSession()
+                }}
+              >
+                Reset
+              </Button>
+            </Box>
+          </Flex>
+        ) : null}
       </Box>
     </AnimatedBox>
   )
@@ -93,35 +142,19 @@ const sections = [
 
 const IndexPage = () => {
   const [section, setSection] = React.useState(0)
-  const [coffee, setCoffee] = React.useState('')
-  const [sessions, setSessions] = useLocalStorage('sessions', [])
+  const [coffee, setCoffee] = React.useState(null)
   const transitions = useTransition(section, p => p, {
     from: {opacity: 0, transform: 'translate3d(200%,0,0)'},
     enter: {opacity: 1, transform: 'translate3d(0%,0,0)'},
     leave: {opacity: 0, transform: 'translate3d(-200%,0,0)'},
   })
-  const handleSubmit = () => {
-    setSessions(
-      sessions.concat({
-        id: sessions.length + 1,
-        coffee: coffee.id,
-      }),
-    )
-  }
+
   return (
     <Layout>
-      <Flex sx={{flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '50vh', mb: 6}}>
+      <Flex sx={{flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '60vh', mb: 6}}>
         {transitions.map(({item, props, key}) => {
           const Section = sections[item]
-          return (
-            <Section
-              key={key}
-              style={props}
-              setSection={setSection}
-              setCoffee={setCoffee}
-              handleSubmit={handleSubmit}
-            />
-          )
+          return <Section key={key} style={props} setSection={setSection} setCoffee={setCoffee} coffee={coffee} />
         })}
       </Flex>
     </Layout>
